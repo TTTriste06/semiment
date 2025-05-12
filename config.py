f62eeabd9ce5871876033493a36cf4c17cd19d03
TTTriste06 <yunhan.yang08@gmail.com>
2025-05-12 15:45:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 # GitHub 配置
 GITHUB_TOKEN_KEY = "GITHUB_TOKEN"  # 用于 st.secrets 获取
-REPO_NAME = "TTTriste06/semi"
+REPO_NAME = "TTTriste06/semiment"
 BRANCH = "main"
 
 # 选择月份
